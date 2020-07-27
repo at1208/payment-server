c25f7fca4751a67499b5591739e50b56be64dbc9
at1208 <amantiwari12081999@gmail.com>
2020-07-27 16:44:27 +0530

    code setup

---
+++ server.js
@@ -0,0 +1,30 @@
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+require('dotenv').config();
+const app = express();
+
+
+//Middleware
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(morgan('dev'));
+
+//DB Server Connection
+mongoose.connect(process.env.DATABASE, {
+          useNewUrlParser: true,
+          useCreateIndex: true,
+          useFindAndModify: false,
+          useUnifiedTopology: true
+    })
+    .then(() => console.log('Connected to DB'))
+    .catch(err => {
+        console.log(err);
+    });
+
+const Port = process.env.PORT || 8000;
+app.listen(Port, () => console.log(`Listening on port ${Port}`))
